ccf916d44bf5d09d00ec88f5610847541d317ad7
Gian Pierree Muñoz <gian0291@gmail.com>
2023-04-26 06:33:47 -0500

    refactor(redis): :fire: refactor redis function

--- src/databases/config.ts
+++ src/databases/config.ts
@@ -1,5 +1,11 @@
 import { createClient } from 'redis';
 
-export const client = createClient();
+export const client = createClient({
+    password: 'NS26PXy62iUVbOMJ3LiBsxMVlw7KHGMn',
+    socket: {
+        host: 'redis-14244.c263.us-east-1-2.ec2.cloud.redislabs.com',
+        port: 14244
+    }
+});
 
 client.on('error', err => console.log('Redis Client Error', err));
